fb366689cde5fd6bf5eb3482cdc35b00f3b5e11e
roman1267 <romanxmaciel@gmail.com>
2023-04-27 18:07:00 -0400

    starting migrating to lazy

--- init.lua
+++ init.lua
@@ -1,105 +1,124 @@
 -- TODO: steal from ~/.config/NvChad
 -- TODO: move smaller configuations into other (for plugins)
 package.path = "/home/romanm/.config/nvim/?.lua;" .. package.path
-require('impatient')
-require('core/init') -- NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-require('plugins')
+--require('core/init') -- NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+local opt = vim.opt
+local g = vim.g
 
+g.mapleader = ' '
+g.maplocalleader = ' '
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup(require('plugins'))
 --[[
 ###################################
 ========== LSP settings. ==========
 ###################################
---]]
-local servers = {
-	rust_analyzer = {},
-	bashls = {},
-	html = {},
-	pylsp = {},
-	lua_ls = {
-		Lua = {
-			workspace = { checkThirdParty = false },
-			telemetry = { enable = false },
-		},
-	},
-}
-require('mason').setup()
-require("mason-lspconfig").setup {
-	ensure_installed = vim.tbl_keys(servers)
-}
-
-local cmp = require 'cmp'
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-
-cmp.event:on(
-	'confirm_done',
-	cmp_autopairs.on_confirm_done()
-)
-
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-require("mason-lspconfig").setup_handlers {
-	-- Custom handlers for specific servers
-	["rust_analyzer"] = function()
-		require("rust-tools").setup {
-			inlay_hints = {
-				-- automatically set inlay hints (type hints)
-				-- default: true
-				auto = true,
-				-- Only show inlay hints for the current line
-				only_current_line = false,
-				-- whether to show parameter hints with the inlay hints or not
-				-- default: true
-				show_parameter_hints = true,
-				-- prefix for parameter hints
-				-- default: "<-"
-				parameter_hints_prefix = "<- ",
-				-- prefix for all the other hints (type, chaining)
-				-- default: "=>"
-				other_hints_prefix = "=> ",
-				-- whether to align to the length of the longest line in the file
-				max_len_align = false,
-				-- padding from the left if max_len_align is true
-				max_len_align_padding = 1,
-				-- whether to align to the extreme right or not
-				right_align = false,
-				-- padding from the right if right_align is true
-				right_align_padding = 7,
-				-- The color of the hints
-				highlight = "Comment",
-			},
-		}
-	end,
-	function(server_name) -- default handler
-		require("lspconfig")[server_name].setup {
-			capabilities = capabilities,
-			settings = servers[server_name]
-		}
-	end
-}
-
--- TODO: TJ Devries video on debugging
--- TODO: Choose between nvim-dap and vimspector--make sure to update packer
-
--------------------- debug adapter --------------------
-local dap = require('dap')
-dap.adapters.lldb = {
-	type = 'executable',
-	command = '/usr/bin/lldb-vscode', -- adjust as needed, must be absolute path
-	name = 'lldb'
-}
-
-dap.configurations.rust = {
-	{
-		name = 'Launch',
-		type = 'lldb',
-		request = 'launch',
-		program = function()
-			return vim.fn.input('Path to executable: ', vim.fn.expand('%:p:h:s?src?target/debug/?'), 'file')
-		end,
-		cwd = '${workspaceFolder}',
-		stopOnEntry = false,
-		args = {},
-	},
-}
+----]]
+--local servers = {
+--	rust_analyzer = {},
+--	bashls = {},
+--	html = {},
+--	pylsp = {},
+--	lua_ls = {
+--		Lua = {
+--			workspace = { checkThirdParty = false },
+--			telemetry = { enable = false },
+--		},
+--	},
+--}
+----require('mason').setup()
+----require("mason-lspconfig").setup {
+----	ensure_installed = vim.tbl_keys(servers)
+----}
+--
+----local cmp = require 'cmp'
+----local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+--
+--cmp.event:on(
+--	'confirm_done',
+--	cmp_autopairs.on_confirm_done()
+--)
+--
+---- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+--local capabilities = vim.lsp.protocol.make_client_capabilities()
+----capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+--
+----require("mason-lspconfig").setup_handlers {
+--	-- Custom handlers for specific servers
+--	--["rust_analyzer"] = function()
+----		require("rust-tools").setup {
+--			inlay_hints = {
+--				-- automatically set inlay hints (type hints)
+--				-- default: true
+--				auto = true,
+--				-- Only show inlay hints for the current line
+--				only_current_line = false,
+--				-- whether to show parameter hints with the inlay hints or not
+--				-- default: true
+--				show_parameter_hints = true,
+--				-- prefix for parameter hints
+--				-- default: "<-"
+--				parameter_hints_prefix = "<- ",
+--				-- prefix for all the other hints (type, chaining)
+--				-- default: "=>"
+--				other_hints_prefix = "=> ",
+--				-- whether to align to the length of the longest line in the file
+--				max_len_align = false,
+--				-- padding from the left if max_len_align is true
+--				max_len_align_padding = 1,
+--				-- whether to align to the extreme right or not
+--				right_align = false,
+--				-- padding from the right if right_align is true
+--				right_align_padding = 7,
+--				-- The color of the hints
+--				highlight = "Comment",
+--			},
+----		}
+--	--end,
+--	function(server_name) -- default handler
+----		require("lspconfig")[server_name].setup {
+--			capabilities = capabilities,
+--			--settings = servers[server_name]
+----		}
+--	--end
+----}
+--
+---- TODO: TJ Devries video on debugging
+---- TODO: Choose between nvim-dap and vimspector--make sure to update packer
+--
+---------------------- debug adapter --------------------
+----local dap = require('dap')
+----dap.adapters.lldb = {
+----	type = 'executable',
+----	command = '/usr/bin/lldb-vscode', -- adjust as needed, must be absolute path
+----	name = 'lldb'
+----}
+--
+----dap.configurations.rust = {
+----	{
+----		name = 'Launch',
+----		type = 'lldb',
+----		request = 'launch',
+----		program = function()
+----			return vim.fn.input('Path to executable: ', vim.fn.expand('%:p:h:s?src?target/debug/?'), 'file')
+----		end,
+----		cwd = '${workspaceFolder}',
+----		stopOnEntry = false,
+----		args = {},
+----	},
+----}
+----
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,212 +2,200 @@ Options = {}
 Options.bufferline = require('lua/plugins/configs/bufferline')
 Options.nvimtree = require('lua/plugins/configs/nvimtree')
 Options.treesitter = require('lua/plugins/configs/treesitter')
-Options.cmp = require('lua/plugins/configs/cmp')
+--Options.cmp = require('lua/plugins/configs/cmp')
 Options.which_key = require('lua/plugins/configs/which_key')
 Options.telescope = require('lua/plugins/configs/telescope')
 Options.nvterm = require('lua/plugins/configs/nvterm')
 Options.lualine = require('lua/plugins/configs/lualine')
 Options.others = require('lua/plugins/configs/others')
 
-return require('packer').startup(function(use)
-  use { "wbthomason/packer.nvim" }
-
-  use { -- Highlight, edit, and navigate code
+return{
+  { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
-    run = function()
+    build = function()
       pcall(require('nvim-treesitter.install').update { with_sync = true })
     end,
     config = function()
       require('nvim-treesitter').setup(Options.treesitter)
     end,
-  }
+  },
 
-  use { -- Additional text objects via treesitter
+  { -- Additional text objects via treesitter
     'nvim-treesitter/nvim-treesitter-textobjects',
-    after = 'nvim-treesitter',
-  }
+    dependencies = 'nvim-treesitter',
+  },
 
   -- highlighting of code comments like 'HACK,' 'BUG,' etc.
-  use {
+  {
     "folke/todo-comments.nvim",
-    requires = "nvim-lua/plenary.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
     config = function()
       require("todo-comments").setup()
     end
-  }
+  },
 
   --TODO: Configuration
-  use 'simrat39/rust-tools.nvim'
+  'simrat39/rust-tools.nvim',
   --TODO: Configuration
-  use 'puremourning/vimspector'
+  'puremourning/vimspector',
   --TODO: Configuration
-  use 'mfussenegger/nvim-dap'
+  'mfussenegger/nvim-dap',
 
-  use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" },
+  { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap" },
     config = function()
       require('dapui').setup()
-    end }
+    end },
 
-  use {
+  {
     "ellisonleao/gruvbox.nvim",
     config = function()
       require('gruvbox').setup(Options.others.gruvbox)
     end
-  }
+  },
 
   --TODO: Configuration
-  use {
+  {
     "windwp/nvim-autopairs",
     config = function()
       require("nvim-autopairs").setup(Options.others.autopairs)
     end
-  }
+  },
 
-  use {
+  {
     "NvChad/nvterm",
     config = function()
       require('nvterm').setup(Options.nvterm)
     end,
-  }
+  },
 
-  use 'lewis6991/impatient.nvim' -- faster nvim loading
+  --'lewis6991/impatient.nvim', -- faster nvim loading
 
   --TODO: Configuration
-  use {
+  {
     'williamboman/mason.nvim',
-    run = ":MasonUpdate",
-  }
+    build = ":MasonUpdate",
+  },
 
   --TODO: Configuration
-  use {
+  {
     'williamboman/mason-lspconfig.nvim',
-  }
+  },
 
   --TODO: Configuration
-  use 'RRethy/vim-illuminate' -- automatic reference highlighting
+  'RRethy/vim-illuminate', -- automatic reference highlighting
 
   --TODO: Configuration
-  use {
+  {
     'folke/neodev.nvim',
     config = function()
       require('neodev').setup({
         library = { plugins = { 'nvim-dap-ui' }, types = true },
       })
     end
-  }
+  },
 
   --TODO: Configuration
-  use { -- LSP Configuration & Plugins
+  { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
-    requires = {
+    dependencies = {
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
 
       'WhoIsSethDaniel/lualine-lsp-progress.nvim',
       'folke/neodev.nvim',
     },
-  }
+  },
 
   --TODO: Configuration
-  use { -- Autocompletion
+  { -- Autocompletion
     'hrsh7th/nvim-cmp',
-    requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'nvim-lspconfig',
+    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'nvim-lspconfig',
       'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline', 'hrsh7th/nvim-cmp' },
     config = function()
       require('cmp').setup(Options.cmp)
     end
-  }
+  },
 
-  use { 'L3MON4D3/LuaSnip', run = 'make install_jsregexp' }
+  { 'L3MON4D3/LuaSnip', build = 'make install_jsregexp' },
 
   --TODO: Configuration
-  use {
+  {
     'nvim-tree/nvim-tree.lua',
     config = function()
       require('nvim-tree').setup(Options.nvimtree)
     end,
-  }
+  },
 
 
   --TODO: Configuration
-  use {
+  {
     "folke/which-key.nvim",
     config = function()
       require('which-key').setup(Options.which_key)
     end
-  }
+  },
 
   --TODO: Configuration
-  use {
-    'akinsho/bufferline.nvim',
-    requires = 'nvim-tree/nvim-web-devicons',
-    config = function()
-      require('bufferline').setup(Options.bufferline)
-    end
-  }
+  --{
+  --  'akinsho/bufferline.nvim',
+  --  dependencies = 'nvim-tree/nvim-web-devicons',
+  --  config = function()
+  --    require('bufferline').setup(Options.bufferline)
+  --  end
+  --},
 
   -- Git related plugins
   --TODO: Configuration
-  use 'tpope/vim-fugitive'
+  'tpope/vim-fugitive',
 
   --TODO: Configuration
-  use {
+  {
     'lewis6991/gitsigns.nvim',
     config = function()
       require('gitsigns').setup(Options.others.gitsigns)
     end
-  }
+  },
 
   -- Add indentation guides even on blank lines
   --TODO: Configuration
-  use {
+  {
     'lukas-reineke/indent-blankline.nvim',
     config = function()
       require('indent_blankline').setup(Options.others.blankline)
     end
-  }
+  },
 
   -- Fancier statusline
-  use {
+  {
     'nvim-lualine/lualine.nvim',
     config = function()
       require('lualine').setup(Options.lualine)
     end
-  }
+  },
 
-  use 'kyazdani42/nvim-web-devicons' -- Icons for lualine
+  'kyazdani42/nvim-web-devicons', -- Icons for lualine
 
   --TODO: Configuration
-  use {
+  {
     'numToStr/Comment.nvim',
     config = function()
       require('Comment').setup(Options.others.comment)
     end
-  }                      -- "gc" to comment visual regions/lines
-  use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+  },                      -- "gc" to comment visual regions/lines
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- Fuzzy Finder (files, lsp, etc)
   --TODO: Configuration
-  use {
+  {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
-    requires = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       require('telescope').setup(Options.telescope)
     end
-  }
+  },
 
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+  -- Fuzzy Finder Algorithm which dependencies local dependencies to be built. Only load if `make` is available
   --TODO: Configuration
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
-
-  -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
-  local has_plugins, plugins = pcall(require, 'custom.plugins')
-  if has_plugins then
-    plugins(use)
-  end
-
-  if is_bootstrap then
-    require('packer').sync()
-  end
-end)
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = vim.fn.executable 'make' == 1 },
+}
