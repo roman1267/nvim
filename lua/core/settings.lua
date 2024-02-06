33ee745ff9b76905c2af88469e11e51466606c3b
Roman1267 <romanxmaciel@gmail.com>
2024-02-06 17:58:00 -0600

    changed misc. settings

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -13,10 +13,10 @@ vim.opt.termguicolors = true
 vim.opt.autoread = true
 vim.opt.mouse = "a"
 vim.opt.breakindent = true
--- opt.tabstop = 2
+vim.opt.tabstop = 4
 vim.opt.expandtab = true
--- opt.softtabstop = 2
--- opt.shiftwidth = 2
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.undofile = true
 vim.opt.autoindent = true
 vim.opt.autochdir = true
@@ -25,11 +25,12 @@ vim.opt.number = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.conceallevel = 1
+vim.opt.conceallevel = 2
 vim.opt.termguicolors = true
 vim.opt.completeopt = "menuone,noselect"
 vim.opt.splitright = true
 vim.opt.splitbelow = false
+vim.opt.spellfile = vim.fn.stdpath("config") .. "/extra/spell/en.UTF-8.add"
 
 -- # Other Vim Options # --
 vim.o.updatetime = 250
