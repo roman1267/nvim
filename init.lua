433886f7d83a280e60411ada9d450baa4f644c0f
Roman1267 <romanxmaciel@gmail.com>
2024-02-19 20:45:22 -0600

    moved bootstrap fn to utils section

--- init.lua
+++ init.lua
@@ -1,17 +1,6 @@
 package.path = "/home/romanm/.config/nvim/?.lua;" .. package.path
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
--- TODO: move to core.utils
-if not vim.loop.fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
-    lazypath })
-end
-
-vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
-
+require("core.settings")
+require("core.utils").bootstrap_or_nil()
 local lazyopts = {
    change_detection = {
       -- automatically check for config file changes and reload the ui
@@ -20,8 +9,6 @@ local lazyopts = {
    },
    defaults = { lazy = true },
 }
-
-require("core.settings")
 require("lazy").setup("plugins", lazyopts)
 require("core.keymaps")
 require("core.autocmd")
--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -18,4 +18,19 @@ utils.lazy_load = function(plugin, opts)
       require(plugin).setup(opts)
    end
 end
+
+utils.bootstrap_or_nil = function()
+   local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+   -- TODO: move to core.utils
+   if not vim.loop.fs_stat(lazypath) then
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
+   end
+
+   vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
+end
+
 return utils
