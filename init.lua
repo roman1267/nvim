bc31a259779c0b13edcbf25e609592ec61e4bc35
Roman1267 <romanxmaciel@gmail.com>
2024-05-14 21:54:28 -0500

    update basic init files to match master

--- init.lua
+++ init.lua
@@ -1,26 +1,6 @@
--- TODO: steal from NvChad
 package.path = "/home/romanm/.config/nvim/?.lua;" .. package.path
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
-    lazypath })
-end
-
-vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
-
-local lazyopts = {
-   change_detection = {
-      -- automatically check for config file changes and reload the ui
-      enabled = true,
-      notify = false, -- get a notification when changes are found
-   },
-}
-
 require("core.init")
-require("lazy").setup("plugins", lazyopts)
 require("core.keymaps")
 require("core.autocmd")
 require("extra")
