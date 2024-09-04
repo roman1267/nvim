9570c7242a0daee71fb6c8fdd6533ba96ea01e20
roman1267 <romanxmaciel@gmail.com>
2024-09-03 23:41:12 -0500

    Add critical loading to main init file

--- init.lua
+++ init.lua
@@ -1,1 +1,9 @@
+-- Require general settings first, then setup of Lazy
+require("config.settings")
 require("config.lazy")
+
+-- Load critical keybinds directly
+require("config.utils").load_mappings("global")
+require("config.utils").load_mappings("buffers")
+require("config.utils").load_mappings("splits")
+require("config.utils").load_mappings("tabs")
