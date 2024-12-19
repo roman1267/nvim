269574f44e5ae6e66e241186f4387b6a74ef972f
roman1267 <romanxmaciel@gmail.com>
2024-12-18 19:38:07 -0600

    Add More Which-Key Groups

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -18,5 +18,15 @@ return {
       "<leader>x",
       group = "Trouble",
     })
+    wk.add({ "<M-i>", desc = "Open floating term" })
+    wk.add({ "<leader>b", group = "Buffer" })
+    wk.add({ "<leader>t", group = "Tab" })
+    wk.add({ "<leader>h", group = "Home" })
+    wk.add({ "<leader>f", group = "Find" })
+    -- Debug group
+    wk.add({ "<leader>d", group = "Debug" })
+    wk.add({ "<leader>ds", group = "Step" })
+    wk.add({ "<leader>db", group = "Breakpoint" })
+    wk.add({ "<leader>s", group = "Split" })
   end,
 }
