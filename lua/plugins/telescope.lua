72d124c398a8059cb43582b4a0fc03bf7d3c3a9a
roman1267 <romanxmaciel@gmail.com>
2024-12-18 19:34:26 -0600

    Telescope + Binds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,4 +2,7 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {},
+  config = {
+    require("config.utils").load_mappings("telescope"),
+  },
 }
