242f81f0a84fe17c29b3b19e3f03e927380f5bdf
roman1267 <romanxmaciel@gmail.com>
2025-01-07 01:32:58 -0600

    add gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,14 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  lazy = false,
+  ft = "gitcommit",
+  opts = {
+    signs = {
+      add = { text = "+" },
+      change = { text = "~" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+    },
+  },
+}
